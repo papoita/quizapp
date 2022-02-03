fd64cfa703c9df959bd7977f5043a167677fc4ad
Tony <awalker780@gmail.com>
2022-02-02 21:29:54 -0500

    adds missing quiz_id column

--- db/schema/04_quiz_attempts.sql
+++ db/schema/04_quiz_attempts.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS quiz_attempts CASCADE;
 CREATE TABLE quiz_attempts (
   id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id),
   score INTEGER,
   date DATE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
