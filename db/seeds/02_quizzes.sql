15f188957f74011e226e894f38c9fb2e1c6f9de9
PaolaP <paolaperez8@gmail.com>
2022-02-03 15:46:24 -0500

    add quiz to test routes

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,2 +1,5 @@
 INSERT INTO quizzes (id, quiz_name, date, user_id, public)
 VALUES (1, 'My General Knowledge Quiz', '2022-02-02', 1, TRUE);
+
+INSERT INTO quizzes (id, quiz_name, date, user_id, public)
+VALUES (2, 'Quiz 2', '2022-02-03', 1, FALSE);
