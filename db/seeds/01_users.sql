39be76a55f78632b162864df7bc4b4e2cf3dd481
Tony <awalker780@gmail.com>
2022-02-03 22:31:17 -0500

    adds more fake data to tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,11 @@
-INSERT INTO users (id, user_name)
-VALUES (1, 'Molly');
+INSERT INTO users (user_name)
+VALUES ('Molly');
+
+INSERT INTO users (user_name)
+VALUES ('Tony');
+
+INSERT INTO users (user_name)
+VALUES ('Paola');
+
+INSERT INTO users (user_name)
+VALUES ('Marcos');
--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -12,3 +12,93 @@ VALUES ('What is the name of the very first video uploaded to YouTube?', 'Tribut
 
 INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
 VALUES ('The heavy metal band Black Sabbath hail from which English city?', 'Manchester', 'London', 'Newcastle-Upon-Tyne', 'Birmingham', 1);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which country inside the United Kingdom does NOT appear on its flag, the Union Jack?', 'Scotland', 'Ireland', 'Isle of Wight', 'Wales', 2);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('What is the name of the first "Star Wars" film by release order?', 'The Phantom Menace', 'The Force Awakens', 'Revenge of the Sith', 'A New Hope', 2);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which of these Disney classics was released in 1970?', 'One Hundred and One Dalmatians', 'The Fox and the Hound', 'The Little Mermaid', 'The Aristocats', 2);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('The Los Angeles Dodgers were originally from what U.S. city?', 'Las Vegas', 'Boston', 'Seattle', 'Brooklyn', 2);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('After India, which country produces the second most movies per year?', 'United States', 'China', 'France', 'Nigeria', 2);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Hippocampus is the Latin name for which marine creature?', 'Dolphin', 'Whale', 'Octopus', 'Seahorse', 3);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which of these songs does NOT play during the Ruins segments of the 2015 game "Undertale?"', 'Anticipation', 'Unnecessary Tension', 'Ruins', 'Another Medium', 3);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('In Super Smash Bros. for Nintendo 3DS and Nintendo Wii U, who was the sixth fighter to be added to the game post-launch?', 'Cloud', 'Bayonnetta', 'Ryu', 'Corrin', 3);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('What is the Zodiac symbol for Gemini?', 'Fish', 'Scales', 'Maiden', 'Twins', 3);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which Dota 1 hero changed gender when ported to Dota 2?', 'Axe', 'Keeper of the Light', 'Templar Assassin', 'Legion Commander', 3);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('What is the "powerhouse" of the Eukaryotic animal cell?', 'Nucleus', 'Chloroplast', 'Endoplasmic Reticulum', 'Mitochondria', 4);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which team won the 2015-16 English Premier League?', 'Liverpool', 'Cheslea', 'Manchester United', 'Leicester City', 4);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which one of these countries borders with Poland?', 'France', 'Norway', 'Netherlands', 'Lithuania', 4);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('From which album is the Gorillaz song, "On Melancholy Hill" featured in?', 'Demon Days', 'Humanz', 'The Fall', 'Plastic Beach', 4);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('In which game did the Konami Code make its first appearance?', 'Contra', 'Castlevania', 'Dance Dance Revolution', 'Gradius', 4);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Who wrote the Sinead O`Connor hit "Nothing Compares 2 U"?', 'Michael Jackson', 'Cameo', 'Rick James', 'Prince', 5);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('EDM producer Marshmello performs live wearing clothes and a marshmallow mask of what colour?', 'Black', 'Blue', 'Yellow', 'White', 5);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('What programming language was used to create the game "Minecraft"?', 'HTML 5', 'C++', 'Python', 'Java', 5);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which actor from The Young Ones also played Lord Flashheart in one episode of Blackadder II?', 'Adrian Edmondson', 'Nigel Planer', 'Christopher Ryan', 'Rik Mayall', 5);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('How many strings are there on a cello?', '5', '6', '8', '4', 5);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which of the following is the world''s best-selling book?', 'The Little Prince', 'Harry Potter and the Philosopher''s Stone', 'The Da Vinci Code', 'The Lord of the Rings', 6);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('On a standard Monopoly board, which square is diagonally opposite "Go"? ', 'Go to Jail', 'Jail', 'The Electric Company', 'Free Parking', 6);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('What block in Minecraft has the highest blast resistance?', 'Obsidian', 'Bedrock', 'Block of Diamond', 'Barrier', 6);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which of the following is a major muscle of the back?', 'Trapezium', 'Trapezoid', 'Triquetrum', 'Trapezius', 6);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which game did NOT get financed via Crowdfunding?', 'Town of Salem', 'Undertale', 'Tower Unite', 'Enter the Gungeon', 6);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which gas forms about 78% of the Earth''s atmosphere?', 'Oxygen', 'Argon', 'Carbon Dioxide', 'Nitrogen', 7);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Josh Mansour is part of what NRL team?', 'Melbourne Storm', 'Sydney Roosters', 'North Queensland Cowboys', 'Penrith Panthers', 7);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which American manufactured submachine gun was informally known by the American soldiers that used it as "Grease Gun"?', 'Colt 9mm', 'Thompson', 'MAC-10', 'M3', 7);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('The Fields Medal, one of the most sought after awards in mathematics, is awarded every how many years?', '3', '5', '6', '4', 7);
+
+INSERT INTO questions (question, answer_1, answer_2, answer_3, answer_correct, quiz_id)
+VALUES ('Which of the following physicists did NOT work on the Manhattan project?', 'Richard Feynman', 'J. Robert Oppenheimer', 'John Von-Neumann', 'Murray Gell-Mann', 7);
