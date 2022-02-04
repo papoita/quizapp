6123b2fe8487b2619bbd1d4624c5d4e7ead448bf
PaolaP <paolaperez8@gmail.com>
2022-02-03 23:22:49 -0500

    fix conflict

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,11 +1,18 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255)
+);
+
+/* previous version
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255)
 );
-/* previous version
+
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
