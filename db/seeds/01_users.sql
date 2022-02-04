89dcf8915dc7bae397245fa8e9d6fc49c882eb34
Tony <awalker780@gmail.com>
2022-02-03 22:39:47 -0500

    changes user_name to username

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,11 @@
-INSERT INTO users (user_name)
+INSERT INTO users (username)
 VALUES ('Molly');
 
-INSERT INTO users (user_name)
+INSERT INTO users (username)
 VALUES ('Tony');
 
-INSERT INTO users (user_name)
+INSERT INTO users (username)
 VALUES ('Paola');
 
-INSERT INTO users (user_name)
+INSERT INTO users (username)
 VALUES ('Marcos');
