7194f078ac68eca38fe17e4e43ca0b472da89200
Tony <awalker780@gmail.com>
2022-02-05 01:06:06 -0500

    add functionality for new questions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,24 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+
+  let counter = 2;
+
+  $("#add-question").on( "click", function( event ) {
+
+    event.preventDefault();
+
+    $("#questions").append(`<section class="question-box">
+    <h2>Question ${counter}</h2>
+    <input type="text" name="question" placeholder="Enter a question">
+    <input type="text" name="correct_answer" placeholder="Correct answer">
+    <input type="text" name="wrong_answer1" placeholder="Wrong answer">
+    <input type="text" name="wrong_answer2" placeholder="Wrong answer">
+    <input type="text" name="wrong_answer3" placeholder="Wrong answer">
+    </section>`)
+
+    counter ++;
+
+  });
+
+});
